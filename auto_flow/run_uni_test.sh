e132c55c16ee0686f7740147960e1730caae846f
starpacker <164129173+starpacker@users.noreply.github.com>
2025-12-11 21:20:52 +0800

    Delete auto_flow directory

--- auto_flow/run_uni_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python uni_test.py \
-  --config-path "config.yaml" \
-  --refactored-json "output/refactored_code.json" \
-  --command "python sim_code.py"  \
-  --working-folder /home/yjh/auto_flow/run_code/ \
-  --code-path /home/yjh/auto_flow/run_code/sim_code.py
